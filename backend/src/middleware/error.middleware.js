ba687212c4bc90839fb06b685d9a485aae5b66c4
bipesh369 <juniorbipesh@gmail.com>
2026-08-29 15:58:30 +0000

    Refactor error handling middleware to improve message assignment for validation errors

--- backend/src/middleware/error.middleware.js
+++ backend/src/middleware/error.middleware.js
@@ -1,12 +1,24 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
 
-  const statusCode = err.statusCode || 500;
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    statusCode = 400;
+
+    if (err.errors?.email) {
+      message = "Invalid email format";
+    } else {
+      message = "Validation failed";
+    }
+  }
 
   res.status(statusCode).json({
     success: false,
-    message: err.message || "Internal Server Error",
+    message,
   });
 };
 
-export default errorHandler
+export default errorHandler;
